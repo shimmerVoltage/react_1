8487720e0e7ace7ec6856412324d6ea37fdcc9f8
shimmerVoltage <shimmerVoltage@SHIMMERVOLTAGE>
2025-08-26 21:40:47 +0300

    DB Jason

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,17 @@ import Header from './header/Header.js';
 import Footer from './footer/Footer.js';
 import Article from './article/Article.js';
 import Nav from './nav/Nav.js';
+import data from './db.json';
 
 function App(props) {
+  let db = data.people;
   return (
     <div className="App">
       <Header />
       <Nav navigation={props.navigation}/>
-      <Article />
-      <Article title = "Современные десктопные процессоры архитектуры x85" content = "Здесь будет содержимое вашей рекламы"/>
+      <Article db = {db} />
+      {/* <Article />
+      <Article title = "Современные десктопные процессоры архитектуры x85" content = "Здесь будет содержимое вашей рекламы"/> */}
       <Footer />
     </div>
   );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,6 +8,7 @@ function Header() {
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1>{title}</h1>
+            <h2>JSON - JavaScript Object Notation</h2>
             <p>
                 Edit <code>src/App.js</code> and save to reload.
             </p>
